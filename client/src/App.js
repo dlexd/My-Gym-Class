39f03bdb6d3b0b6948b5a0a91377b4c243148d72
bentto90 <cch3056@gmail.com>
2023-06-05 16:52:52 -0700

    /deleted uncleared file structure and changed dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -7,73 +7,28 @@ import {
   createHttpLink,
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
+import './App.css';
 
 import Home from './pages/Home';
-import Detail from './pages/Detail';
-import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
-import Nav from './components/Nav';
-import { StoreProvider } from './utils/GlobalState';
-import OrderHistory from './pages/OrderHistory';
+import Navbar from './components/Nav';
+import Footer from './components/Footer';
+import Nutrition from './pages/nutrition';
+import Exercise from './pages/exercise';
 
-const httpLink = createHttpLink({
-  uri: '/graphql',
-});
+const app = () => {
+  <Box width="400px" sx={{ width: { xl: '1488px'}}} m="auto">
+    <Navbar />
+    <Routes>
+      <Route path ="/" element={<Home />} />
+      <Route path ="/login" element={<Login />} />
+      <Route path ="/signup" element={<Signup />} />
+      <Route path ="/exercise/:id" element={<Exercise />} />
+      <Route path ="/nutrition/:id" element={<Nutrition />} />
+    </Routes>
+    <Footer />
+  </Box>
+};
 
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
-
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
-
-function App() {
-  return (
-    <ApolloProvider client={client}>
-      <Router>
-        <div>
-          <StoreProvider>
-            <Nav />
-            <Routes>
-              <Route 
-                path="/" 
-                element={<Home />} 
-              />
-              <Route 
-                path="/login" 
-                element={<Login />} 
-              />
-              <Route 
-                path="/signup" 
-                element={<Signup />} 
-              />
-              <Route 
-                path="/orderHistory" 
-                element={<OrderHistory />} 
-              />
-              <Route 
-                path="/products/:id" 
-                element={<Detail />} 
-              />
-              <Route 
-                path="*"
-                element={<NoMatch />} 
-              />
-            </Routes>
-          </StoreProvider>
-        </div>
-      </Router>
-    </ApolloProvider>
-  );
-}
-
-export default App;
+export default app;
