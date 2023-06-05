8f5869b92dda042d685ce09d910aec6d565aa08c
bentto90 <cch3056@gmail.com>
2023-06-05 13:08:41 -0700

    /added nav components and file structures

---
+++ client/src/component/nav/Navbar.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Auth } from '../../utils/auth';
+
+function Navbar() {
+
+    function Navigation() {
+        if (Auth.loggedIn()) {
+            return (
+                <ul className="flex-row">
+                    <li className="mx-1">
+                        <Link to="/nutrition">
+                            Nutrition
+                        </Link>
+                    </li>
+                    <li className="mx-1">
+                        <a href="/" onClick={() => Auth.logout()}>
+                            Logout
+                        </a>
+                    </li>
+                </ul>
+            );
+        } else {
+            return (
+                <ul className="flex-row">
+                    <li className="mx-1">
+                        <Link to="/signup">
+                            Signup
+                        </Link>
+                    </li>
+                    <li className="mx-1">
+                        <Link to="/login">
+                            Login
+                        </Link>
+                    </li>
+                </ul>
+            );
+        }
+    }
+
+    return (
+        <header className="flex-row px-1">
+            <h1>
+                <Link to="/">
+                    <span role="img" aria-label="fitness">🏋️‍♂️</span>
+                    TEMPORARY NAME
+                </Link>
+            </h1>
+
+            <nav>
+                <Navigation />
+            </nav>
+        </header>
+    );
+}
+
+export default Navbar;
+
+    
+
+
