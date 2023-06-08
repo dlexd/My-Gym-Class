839883ad89d9c0c681eb582f6f890a18a64e7313
bentto90 <cch3056@gmail.com>
2023-06-08 15:17:47 -0700

    /signup function added with selection

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -66,6 +66,69 @@ function Signup(props) {
                         onChange={handleChange}
                     />
                 </div>
+
+                <div className='flex-row space-between my-2'>
+                    <label htmlFor="pwd">Confirm Password:</label>
+                    <input
+                        placeholder="******"
+                        name="password"
+                        type="password"
+                        id="pwd"
+                        onChange={handleChange}
+                    />
+                </div>
+
+                <div className='flex-row space-between my-2'>
+                    <label htmlFor="weight">Weight:</label>
+                    <input
+                        placeholder="Weight"
+                        name="weight"
+                        type="weight"
+                        id="weight"
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className='flex-row space-between my-2'>
+                    <label htmlFor="height">Height:</label>
+                    <input
+                        placeholder="Height"
+                        name="height"
+                        type="height"
+                        id="height"
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className='flex-row space-between my-2'>
+                    <label htmlFor="age">Age:</label>
+                    <input
+                        placeholder="Age"
+                        name="age"
+                        type="age"
+                        id="age"
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className='flex-row space-between my-2'>
+                    <label htmlFor="Activity"
+                    >Activity Level:</label>
+                    <select name="Activity" id="Activity" onChange={handleChange}>
+                        <option value="sedentary">Sedentary</option>
+                        <option value="light">Light</option>
+                        <option value="moderate">Moderate</option>
+                        <option value="very">Very</option>
+                        <option value="extra">Extra</option>
+                    </select>
+                </div>
+                <div className='flex-row space-between my-2'>
+                    <label htmlFor="Goal"
+                    >Goal:</label>
+                    <select name="Goal" id="Goal" onChange={handleChange}>
+                        <option value="lose">Lose</option>
+                        <option value="maintain">Maintain</option>
+                        <option value="gain">Gain</option>
+                    </select>
+                </div>
+                
                 <div className="flex-row flex-end">
                     <button type="submit">
                         Submit
